cba72c90e81def2d57bf21be034c057f849d0508
Leela karthikeyan <leelakarthikeyan13@gmail.com>
2022-11-03 22:36:14 +0530

    update 4.0

--- actions/login.php
+++ actions/login.php
@@ -8,17 +8,31 @@ $password=$_POST['password_log'];
 $sql = "Select * from `userdata` where useremail = '$useremail'  and password ='$password'";
 $result = mysqli_query($con,$sql);
 //if no of rows is greater than 0 then data is present in base
-if(mysqli_num_rows($result)==1){
+
+$mailAdmin = "mainAdmin@ovp.in";  //only one admin address is used in our site -- not stored in DB it is default
+$passAdmin = "asdfgh@zxcvb@123";
+ //to check whether admin is logging in
+if((strpos($mailAdmin, $useremail) !== false) && ((strpos($passAdmin, $password) !== false))){ //enter admin dash
     echo '<script>
-    window.location="../userDashboard/userdash.php";
+    window.location="../admin/mainPage.php"; 
     </script>';
-}
+
+} 
 else{
-    echo '<script>
-    alert("Invalid credentials");
-    window.location="../partials/login.php";
-    </script>';
+    if(mysqli_num_rows($result)==1){
+        echo '<script>
+        window.location="../userDashboard/userdash.php";
+        </script>';
+    }
+    else{
+        echo '<script>
+        alert("Invalid credentials");
+        window.location="../partials/login.php";
+        </script>';
+    }
 }
+
+
 
 //to use in dashboard
 // $data=mysqli_fetch_array($result);
